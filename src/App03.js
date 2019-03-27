3803456ccfa4711cc75519b1b85a90b604243734
Hans Fast <hans@surfacemarkup.net>
2019-03-27 09:55:17 +0100

    example 03

---
+++ src/App03.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      liked: false
+    };
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({
+      liked: this.state.liked?false:true
+    });
+  }
+  render() {
+    const buttonText = this.state.liked?'Unlike':'Like';
+    const buttonClassName = this.state.liked?'liked':'';
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <button
+          onClick={this.handleChange}
+          className={buttonClassName}>
+          {buttonText}
+        </button>
+      </div>
+    );
+  }
+}
+
+export default App;
