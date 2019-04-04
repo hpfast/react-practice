662508d1c382c4dcb696aff96b40f550c0588ab1
Hans Fast <hans@surfacemarkup.net>
2019-04-04 12:09:51 +0200

    add example 08: firebase

---
+++ src/App08.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+import firebase from 'firebase';
+import 'firebase/firestore';
+import logo from './logo.svg';
+import './App.css';
+import './05-list.css';
+
+
+const config = {
+    apiKey: process.env.REACT_APP_APIKEY,
+    authDomain: "email-box-78872.firebaseapp.com",
+    databaseURL: "https://email-box-78872.firebaseio.com",
+    projectId: "email-box-78872",
+    storageBucket: "email-box-78872.appspot.com",
+    messagingSenderId: "660257929986"
+  };
+firebase.initializeApp(config);
+
+
+class InboxItem extends Component {
+  render() {
+    return (
+    <p key={this.props.idx} className="email-list-row">
+      <span>{this.props.item.sender}</span>
+      <span>{this.props.item.subject}</span>
+    </p>
+    )
+  }
+}
+
+class Inbox extends Component {
+  render() {
+    const body = this.props.fetching_data? 'loading ...' : this.props.data.map((item, idx) => <InboxItem idx={idx} item={item} read={item.read}/>);
+    return (
+      <div className="email-list">
+      <p className="email-list-header">
+        <span>Sender</span><span>Subject</span>
+      </p>
+      {body}
+      </div>
+      )
+  }
+
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.db = firebase.firestore();
+    this.state = {
+      messages: [],
+      fetching_data: true
+    };
+  }
+
+  componentDidMount() {
+    this.db.collection('emails').get().then(snapshot => {
+      let data = snapshot.docs.map(doc => doc.data());
+      this.setState({
+        messages:data,
+        fetching_data: false
+      })
+    },err => console.log(err));
+    //fetch("https://raw.githubusercontent.com/hpfast/react-practice/master/messages.json")
+    //.then(response => response.json())
+    //.then(data => this.setState({
+        //messages: data.messages,
+        //fetching_data: false
+      //})
+    //)
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Inbox</h2>
+        </header>
+        <Inbox fetching_data={this.state.fetching_data} data={this.state.messages}/>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App07';
+import App from './App08';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
